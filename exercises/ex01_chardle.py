1ea1fff81d30fb274a81ce361db2e1b0d3e8619e
Kambree Vincent <kamkam@ad.unc.edu>
2022-08-29 19:56:17 -0400

    progress on EX01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -21,10 +21,8 @@ if len(first_character) < 1:
 
 char_count: str = (sum(char == first_character for char in first_word))
 
-print("Searching for " + first_character + " in " + first_word) 
+print("Searching for " + first_character + " in " + first_word)  
 
-def stringify(num: int) -> str: 
-    return str(num)
 
 if first_character == first_word[0]:
     print(first_character + " found at index 0 ")
@@ -42,20 +40,20 @@ if first_character == first_word[4]:
     print(first_character + " found at index 4 ")
 
 
-if char_count == 0:
+if char_count == (0):
     print(" No instances of " + first_character + " found in " + first_word) 
 
-if char_count == 1: 
+if char_count == (1): 
     print(" 1 instance of " + first_character + " found in " + first_word)
 
-if char_count == 2: 
+if char_count == (2): 
     print(" 2 instances of " + first_character + " found in " + first_word) 
    
-if char_count == 3: 
+if char_count == (3): 
     print(" 3 instances of " + first_character + " found in " + first_word)
 
-if char_count == 4: 
+if char_count == (4): 
     print(" 4 instances of " + first_character + " found in " + first_word)
 
-if char_count == 5: 
+if char_count == (5): 
     print(" 5 instances of " + first_character + " found in " + first_word)
