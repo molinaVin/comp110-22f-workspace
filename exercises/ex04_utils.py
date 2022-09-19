7895816b115f4b92dfb106ed0a42096bef8159a6
Kambree Vincent <kamkam@ad.unc.edu>
2022-09-19 11:59:31 -0400

    progress on ex04

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,30 @@
+"""EX04_utils!!!"""
+__author__ = "730409415"
+
+from random import input  #this may not do anything, but doesn't look like im able to input some values)
+
+a_list: list[int] = list()
+def all(input:list[int], int) -> bool:
+    if list[int] == int():
+        return True 
+    else: 
+        return False
+
+
+def max(input:list[int]) -> int: 
+    if len(input)==0:
+        raise ValueError("max() arg is an empty List")
+    else:
+        return max()
+
+
+list_one: input(int) 
+list_two: input(int)
+#do individual parameters need to be defined first? doesnt look like it is considering it as a variable below...)
+def is_equal(list_one, list_two) -> int:
+    if list_one == list_two:
+        return True 
+    else: 
+        return False
+
+#does this code need a condition to ensure it isnt infinte? probably.. not sure where though? i = ? 
